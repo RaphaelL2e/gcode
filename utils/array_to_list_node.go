51d39d7927f63ef0f4a65aa1e9762bc8f58b8f74
RaphaelL2e <1042245208@qq.com>
2023-03-25 22:29:30 +0800

    feat: utils: array convert to list node

--- utils/array_to_list_node.go
+++ utils/array_to_list_node.go
@@ -1,1 +1,25 @@
 package utils
+
+import . "gcode/model"
+
+func ArrayToListNode(arr []int) *ListNode {
+	dummy := &ListNode{}
+	cur := dummy
+	for _, val := range arr {
+		node := &ListNode{
+			Val: val,
+		}
+		cur.Next = node
+		cur = cur.Next
+	}
+	return dummy.Next
+}
+
+func ListNodeToArray(node *ListNode) []int {
+	var res []int
+	for node != nil {
+		res = append(res, node.Val)
+		node = node.Next
+	}
+	return res
+}
--- utils/array_to_list_node_test.go
+++ utils/array_to_list_node_test.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"reflect"
 	"testing"
+
+	. "gcode/model"
 )
 
 func TestArrayToListNode(t *testing.T) {
@@ -14,7 +16,13 @@ func TestArrayToListNode(t *testing.T) {
 		args args
 		want *ListNode
 	}{
-		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{
+				arr: []int{3, 2, 0, 4},
+			},
+			want: ArrayToListNode([]int{3, 2, 0, 4}),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -34,7 +42,13 @@ func TestListNodeToArray(t *testing.T) {
 		args args
 		want []int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{
+				node: ArrayToListNode([]int{3, 2, 0, 4}),
+			},
+			want: []int{3, 2, 0, 4},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
